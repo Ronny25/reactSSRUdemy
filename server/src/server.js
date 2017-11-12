b0b996e8dccde3c836d8e88ddf3035708fe28504
Dmytro Kotkin <kotkin.lg@gmail.com>
2017-11-12 19:08:21 +0200

    Express passes all the http requests to ReactRouter.
    Signed-off-by: Dmytro Kotkin <kotkin.lg@gmail.com>

--- server/src/server.js
+++ server/src/server.js
@@ -4,9 +4,9 @@ import renderer from './helpers/renderer';
 const app = express();
 
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.send(renderer(req));
-});
+app.get('*', (req, res) => (
+  res.send(renderer(req))
+));
 
 app.listen(3000, () => (
   console.log('Listening on port 3000')
