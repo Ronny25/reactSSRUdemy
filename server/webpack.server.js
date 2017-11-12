922b4b4389cb9ae45e645f06d5d30ed171c75cbd
Dmytro Kotkin <kotkin.lg@gmail.com>
2017-11-12 18:21:48 +0200

    Script npm-run-all, webpack-node-externals.
    Signed-off-by: Dmytro Kotkin <kotkin.lg@gmail.com>

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   // Inform webpack we're building a bundle
@@ -9,14 +10,16 @@ const config = {
 
   // Tell webpack the root file of our
   // server application
-  entry: './src/index.js',
+  entry: './src/server.js',
 
   // Tell webpack where to put the output file
   // that is generated
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
