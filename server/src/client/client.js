b71fc99cebb47c9bb1354a31509832697c4304b5
Dmytro Kotkin <kotkin.lg@gmail.com>
2017-11-19 14:43:03 +0200

    Create server and client Redux stores.
    Signed-off-by: Dmytro Kotkin <kotkin.lg@gmail.com>

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,12 +1,20 @@
 // Starting point for the client side application
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
 import Routes from './Routes';
 
-ReactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+hydrate(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root')
 );
