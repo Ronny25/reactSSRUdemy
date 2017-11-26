4b411df01b6f1f840289ede1f64018997a81a88c
Dmytro Kotkin <kotkin.lg@gmail.com>
2017-11-26 18:20:32 +0200

    Add a call of an auth method.
    Signed-off-by: Dmytro Kotkin <kotkin.lg@gmail.com>

--- server/src/client/App.js
+++ server/src/client/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { renderRoutes } from 'react-router-config';
 
+import { fetchCurrentUser } from './actions';
 import Header from './components/Header';
 
 const App = ({ route }) => (
@@ -11,5 +12,6 @@ const App = ({ route }) => (
 );
 
 export default {
-  component: App
+  component: App,
+  loadData: ({ dispatch }) => dispatch(fetchCurrentUser())
 };
