41cb89fd09884b8938a7d2ae6a101f1db49dfcdd
Dmytro Kotkin <kotkin.lg@gmail.com>
2017-11-26 18:46:24 +0200

    Add some authentication magic.
    Signed-off-by: Dmytro Kotkin <kotkin.lg@gmail.com>

--- server/src/client/components/Header.js
+++ server/src/client/components/Header.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const Header = () => (
-  <div>
-    <Link to="/">React SSR</Link>
-  </div>
-);
+const Header = ({ auth }) => {
+  const authButton = auth ? (
+    <a href="/api/logout">Logout</a>
+  ) : (
+    <a href="/api/auth/google">Login</a>
+  );
+
+  return (
+    <div>
+      <Link to="/">React SSR</Link>
+      <div>
+        <Link to="/users">Users</Link>
+        <Link to="/admins">Admins</Link>
+        {authButton}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = ({ auth }) => ({ auth });
 
