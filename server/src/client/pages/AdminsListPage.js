07a26cf9a5ae88572e3cb64b8280d6ff3e849a17
Dmytro Kotkin <kotkin.lg@gmail.com>
2017-12-03 15:35:28 +0200

    Implement requireAuth method.
    Signed-off-by: Dmytro Kotkin <kotkin.lg@gmail.com>

--- server/src/client/pages/AdminsListPage.js
+++ server/src/client/pages/AdminsListPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchAdmins } from '../actions/index';
+import requireAuth from '../components/hocs/requireAuth';
 
 class AdminsListPage extends Component {
   componentDidMount() {
@@ -28,6 +29,8 @@ const mapStateToProps = ({ admins }) => ({ admins });
 // const loadData = store => store.dispatch(fetchAdmins());
 
 export default {
-  component: connect(mapStateToProps, { fetchAdmins })(AdminsListPage),
+  component: connect(mapStateToProps, { fetchAdmins })(
+    requireAuth(AdminsListPage)
+  ),
   loadData: ({ dispatch }) => dispatch(fetchAdmins())
 };
